81f56c7afe623d56433518619b22a4b909cfd0c1
Shadowss <cata7007@gmail.com>
2013-10-28 10:45:23 +0200

    update admin , delete server update

--- Admin/admin.php
+++ Admin/admin.php
@@ -97,7 +97,12 @@ $up_avl = $latest - $ver ;
 												<a href="<?php echo HOMEPAGE; ?>">Server Homepage</a>
 												<a href="admin.php">Control Panel Home</a>
 												<a href="<?php echo SERVER; ?>dorf1.php">Return to the server</a>
+												<?php
+												/*
 												<a href="?p=update"><font color="Red"><b>Server Update (<?php echo $up_avl; ?>)</font></b></a>
+												<br />
+												*/
+												?>
 												<br />
 												<a href="?action=logout">Logout</a>
 												<br />
