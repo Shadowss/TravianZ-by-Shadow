3d96b475c6a5a1551023d0ff2f86e67243f575f4
Shadow <cata7007@gmail.com>
2013-11-10 10:57:31 +0200

    Update Generator.php

--- GameEngine/Generator.php
+++ GameEngine/Generator.php
@@ -106,8 +106,9 @@ public function procMtime($time, $pref = 3) {
 			break;
 		}
 		*/
--       $time += 3600*0; //Edit this yourself
-+       $time += 0; //Edit this yourself
+//-       $time += 3600*0; //Edit this yourself
+//+       $time += 0; //Edit this yourself
+$time -= 3600*TIMEZONE; //Edit this yourself
 
 $today = date('d',time())-1;
 if (date('Ymd',time()) == date('Ymd',$time)) {
