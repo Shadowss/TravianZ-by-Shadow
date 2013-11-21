ae814295c36af4049307a3afa95bd92811870d61
Shadow <cata7007@gmail.com>
2013-11-21 14:51:46 +0200

    Update Village.php

--- GameEngine/Village.php
+++ GameEngine/Village.php
@@ -26,22 +26,15 @@ class Village {
 	private $production = array();
 	private $oasisowned,$ocounter = array();
 
-    	function Village() {
-        	global $session, $database;
-        	if(isset($_SESSION['wid'])) {
-            	$this->wid = $_SESSION['wid'];
-            
-        	}
-        	else {
-            $this->wid = $session->villages[0];
-        }
-        //add new line code
-        //check exist village if from village destroy to avoid error msg.
-        if (!$database-> checkVilExist($this->wid)) {
-            $this->wid=$database->getVillageID($session->uid);
-            $_SESSION['wid']=$this->wid;
-        }
-        	$this->LoadTown();
+	function Village() {
+		global $session;
+		if(isset($_SESSION['wid'])) {
+			$this->wid = $_SESSION['wid'];
+		}
+		else {
+			$this->wid = $session->villages[0];
+		}
+		$this->LoadTown();
 		$this->calculateProduction();
 		$this->processProduction();
 		$this->ActionControl();
@@ -73,7 +66,6 @@ class Village {
 		$this->unitarray = $database->getUnit($this->wid);
 		$this->enforcetome = $database->getEnforceVillage($this->wid,0);
 		$this->enforcetoyou = $database->getEnforceVillage($this->wid,1);
-		$this->enforceoasis = $database->getOasisEnforce($this->wid,0);
 		$this->unitall =  $technology->getAllUnits($this->wid);
 		$this->techarray = $database->getTech($this->wid);
 		$this->abarray = $database->getABTech($this->wid);
